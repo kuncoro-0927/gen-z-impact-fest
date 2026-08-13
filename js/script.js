01bd79691cf5a444920d46af0b8cf1878c4de6d4
kuncoro-0927 <khitanhesthik@gmail.com>
2026-08-13 11:56:43 +0700

    mobile responsive

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const openBtn = document.getElementById("menuOpenBtn");
+const closeBtn = document.getElementById("menuCloseBtn");
+const menu = document.getElementById("mobileMenu");
+const backdrop = document.getElementById("menuBackdrop");
+
+function openMenu() {
+  menu.classList.remove("-translate-x-[calc(100%+16px)]");
+  backdrop.classList.remove("opacity-0", "pointer-events-none");
+  backdrop.classList.add("opacity-100");
+  document.body.classList.add("overflow-hidden");
+}
+
+function closeMenu() {
+  menu.classList.add("-translate-x-[calc(100%+16px)]");
+  backdrop.classList.add("opacity-0", "pointer-events-none");
+  backdrop.classList.remove("opacity-100");
+  document.body.classList.remove("overflow-hidden");
+}
+
+openBtn.addEventListener("click", openMenu);
+closeBtn.addEventListener("click", closeMenu);
+backdrop.addEventListener("click", closeMenu);
+
+const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
+
+mobileNavLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const targetId = link.getAttribute("href");
+    const targetSection = document.querySelector(targetId);
+
+    closeMenu();
+
+    if (targetSection) {
+      setTimeout(() => {
+        targetSection.scrollIntoView({ behavior: "smooth" });
+      }, 300);
+    }
+  });
+});
